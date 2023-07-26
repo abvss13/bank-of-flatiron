0ccd6a746353cdac1eeac7d56f22bd4e6248c65d
Abdullahi Abass <135229942+abvss13@users.noreply.github.com>
2023-07-26 11:10:12 +0300

    Update TransctionApp.js

--- bank-of-flatiron/src/TransctionApp.js
+++ bank-of-flatiron/src/TransctionApp.js
@@ -1,109 +1,105 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
+import './index.css';
+import './App.css'; 
 
 const TransactionApp = () => {
   const [transactions, setTransactions] = useState([]);
-
-  const fetchTransactions = async () => {
-    try {
-      const response = await axios.get('https://api.npoint.io/24f85e9a6815d12cf771');
-      setTransactions(response.data.transactions);
-    } catch (error) {
-      console.error('Error fetching transactions:', error);
-    }
-  };
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newTransaction, setNewTransaction] = useState({
+    description: '',
+    amount: '',
+    category: '',
+  });
 
   useEffect(() => {
     fetchTransactions();
   }, []);
 
-  const addTransaction = (newTransaction) => {
-    setTransactions([...transactions, newTransaction]);
-  };
+  const fetchTransactions = async () => {
+    try {
+      const response = await axios.get('https://api.npoint.io/9a6438df7be8936787f7');
+      // If the transactions are nested inside an object, extract them here
+      const transactionsArray = response.data.transactions || response.data;
 
-  const filterTransactions = (searchTerm) => {
-    if (!searchTerm) {
-      fetchTransactions();
-    } else {
-      const filteredTransactions = transactions.filter((transaction) =>
-        transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setTransactions(filteredTransactions);
+      setTransactions(transactionsArray);
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
     }
   };
 
-  const sortTransactions = (sortBy) => {
-    const sortedTransactions = [...transactions].sort((a, b) => {
-      if (sortBy === 'category') {
-        return a.category.localeCompare(b.category);
-      } else if (sortBy === 'description') {
-        return a.description.localeCompare(b.description);
-      }
-      return 0;
-    });
-    setTransactions(sortedTransactions);
+  const handleAddTransaction = () => {
+    // Add the newTransaction to the transactions state
+    setTransactions([...transactions, newTransaction]);
+
+    // Clear the form fields after adding a new transaction
+    setNewTransaction({ description: '', amount: '', category: '' });
   };
 
-  const deleteTransaction = (transactionId) => {
-    const updatedTransactions = transactions.filter((transaction) => transaction.id !== transactionId);
-    setTransactions(updatedTransactions);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Get the form data 
-    const description = e.target.elements.description.value;
-    const amount = e.target.elements.amount.value;
-    // Generate a new transaction object 
-    const newTransaction = {
-      id: transactions.length + 1,
-      description,
-      amount: parseFloat(amount),
-    };
-    // Add the new transaction to the table
-    addTransaction(newTransaction);
-    // Clear the form inputs
-    e.target.reset();
-  };
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <div className="container">
-      <header>
-        <h1>Bank of Flatiron</h1>
-      </header>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="description" placeholder="Description" />
-        <input type="number" name="amount" placeholder="Amount" />
-        <button type="submit">Add Transaction</button>
-      </form>
-
+    <div>
+      {/* SearchBar component */}
       <input
         type="text"
         placeholder="Search transactions..."
-        onChange={(e) => filterTransactions(e.target.value)}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
 
-      <button onClick={() => sortTransactions('category')}>Sort by Category</button>
-      <button onClick={() => sortTransactions('description')}>Sort by Description</button>
+      {/* TransactionForm component */}
+      <div>
+        <input
+          type="text"
+          placeholder="Description"
+          value={newTransaction.description}
+          onChange={(e) =>
+            setNewTransaction({ ...newTransaction, description: e.target.value })
+          }
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={newTransaction.amount}
+          onChange={(e) => setNewTransaction({ ...newTransaction, amount: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Category"
+          value={newTransaction.category}
+          onChange={(e) => setNewTransaction({ ...newTransaction, category: e.target.value })}
+        />
+        <input
+          type="number"
+          placeholder="Date"
+          value={newTransaction.date}
+          onChange={(e) =>
+            setNewTransaction({ ...newTransaction, date: e.target.value })
+          }
+        />
+        <button onClick={handleAddTransaction}>Add Transaction</button>
+      </div>
 
+      {/* TransactionTable component */}
       <table>
         <thead>
           <tr>
             <th>Description</th>
             <th>Amount</th>
-            <th>Action</th>
+            <th>Category</th>
+            <th>date</th>
           </tr>
         </thead>
         <tbody>
-          {transactions.map((transaction) => (
+          {filteredTransactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.description}</td>
               <td>{transaction.amount}</td>
-              <td>
-                <button onClick={() => deleteTransaction(transaction.id)}>Delete</button>
-              </td>
+              <td>{transaction.category}</td>
+              <td>{transaction.date}</td>
             </tr>
           ))}
         </tbody>
